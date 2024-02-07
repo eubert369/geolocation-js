e8efb24642bc92ae7105409b1bad1b87a9c5c623
MEC369 <marceubert886@gmail.com>
2024-02-07 17:34:13 +0800

    current location with google map api

--- app.js
+++ app.js
@@ -1,38 +1,50 @@
 
-var myAPIKey = "ebe23a3f33c441faa7078ef5c1eb0cda";
+// var myAPIKey = "ebe23a3f33c441faa7078ef5c1eb0cda";
 
-var map = new maplibregl.Map({
-    container: 'map',
-      style: `https://maps.geoapify.com/v1/styles/klokantech-basic/style.json?apiKey=${myAPIKey}`,
-    // style: 'https://maps.geoapify.com/v1/tile/carto/{z}/{x}/{y}.png?&apiKey=ebe23a3f33c441faa7078ef5c1eb0cda',
-});
-map.addControl(new maplibregl.NavigationControl());
+// var map = new maplibregl.Map({
+//     container: 'map',
+//       style: `https://maps.geoapify.com/v1/styles/klokantech-basic/style.json?apiKey=${myAPIKey}`,
+//     // style: 'https://maps.geoapify.com/v1/tile/carto/{z}/{x}/{y}.png?&apiKey=ebe23a3f33c441faa7078ef5c1eb0cda',
+// });
+// map.addControl(new maplibregl.NavigationControl());
 
 
 
 // JS Geolocation
 
-// const successCallback = (position) => {
-//     console.log(position);
-//     document.getElementById('lat').textContent = 'Latitude: ' + position.coords.latitude;
-//     document.getElementById('long').textContent = 'Longitude: ' + position.coords.longitude;
-//     document.getElementById('acc').textContent = 'Accuracy: ' + position.coords.accuracy;
+var gmpMap = document.querySelector('gmp-map');
+var gmpMarker = document.querySelector('gmp-advanced-marker');
 
-//     if (position.coords.accuracy <= 100) {
-//         console.log('coordinates within 100 meters');
-//     } else {
-//         console.log('cant detect coordinates within 100 meters');
-//     }
-// };
+// var gmpCenterAttribute = gmpMap.getAttribute('center');
+// var gmpMarkerPositionAttribute = gmpMarker.getAttribute('position');
 
-// const errorCallback = (error) => {
-//     console.log(error);
-// };
+var coordinates = '';
 
-// const options = {
-//     enableHighAccuracy: true,
-//     timeout: 10000,
-// };
+const successCallback = (position) => {
+    console.log(position);
+    coordinates = position.coords.latitude + ', ' + position.coords.longitude;
+    gmpMap.setAttribute('center', coordinates);
+    gmpMarker.setAttribute('position', coordinates);
+    
+    // document.getElementById('lat').textContent = 'Latitude: ' + position.coords.latitude;
+    // document.getElementById('long').textContent = 'Longitude: ' + position.coords.longitude;
+    // document.getElementById('acc').textContent = 'Accuracy: ' + position.coords.accuracy;
 
-// navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
-// // navigator.geolocation.watchPosition(successCallback, errorCallback);
+    if (position.coords.accuracy <= 100) {
+        console.log('coordinates within 100 meters');
+    } else {
+        console.log('cant detect coordinates within 100 meters');
+    }
+};
+
+const errorCallback = (error) => {
+    console.log(error);
+};
+
+const options = {
+    enableHighAccuracy: true,
+    timeout: 10000,
+};
+
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
+// navigator.geolocation.watchPosition(successCallback, errorCallback);
